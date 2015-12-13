1bc1b6e31696add87571abf0ba0154cb44fb85c9
Jay Versluis <jay@versluis.com>
2015-12-13 09:14:46 -0500

    added example for local and remote URLs

--- Movie Player/ViewController.m
+++ Movie Player/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+@import AVFoundation;
+@import AVKit;
 
 @interface ViewController ()
 
@@ -16,12 +18,41 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // grab a local URL to our video
+    NSURL *videoURL = [[NSBundle mainBundle]URLForResource:@"video" withExtension:@"mp4"];
+    
+    // or use a remote URL
+    // NSURL *videoURL = [NSURL URLWithString:@"https://fpdl.vimeocdn.com/vimeo-prod-skyfire-std-us/01/4440/5/147203742/446020536.mp4?token=566d8865_0x4f7dd335901c1ab42317c3d0b8d237db988c4e98&download=1&filename=Presenting+on+top+of+a+UISplitView+Controller+-+Part+1-HD.mp4"];
+    
+    // create an AVPlayer
+    AVPlayer *player = [AVPlayer playerWithURL:videoURL];
+    
+    // create a player view controller
+    AVPlayerViewController *controller = [[AVPlayerViewController alloc]init];
+    controller.player = player;
+    [player play];
+    
+    // show the view controller
+    [self addChildViewController:controller];
+    [self.view addSubview:controller.view];
+    controller.view.frame = self.view.frame;
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (UIViewController*) topMostController {
+    UIViewController *topController = [UIApplication sharedApplication].keyWindow.rootViewController;
+    
+    while (topController.presentedViewController) {
+        topController = topController.presentedViewController;
+    }
+    
+    return topController;
 }
 
 @end
