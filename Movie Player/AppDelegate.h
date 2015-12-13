ebd47188e200e437604deaa104c749977e7d1159
Jay Versluis <jay@versluis.com>
2015-12-13 08:36:57 -0500

    Initial Commit

---
+++ Movie Player/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Movie Player
+//
+//  Created by Jay Versluis on 13/12/2015.
+//  Copyright © 2015 Pinkstone Pictures LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
